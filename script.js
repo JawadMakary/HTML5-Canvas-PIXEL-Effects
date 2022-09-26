e140bb150b14864cae5e5c6df66af7d861f43229
jawad halby makary <jawadhalbymakary@gmail.com>
2022-09-26 22:36:40 +0300

    added dynamic particles animation

--- script.js
+++ script.js
@@ -4,17 +4,24 @@ window.addEventListener("load", function () {
   // getContext is used for HTML element canvas return instance of obj canvasrenderingcontext2d
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
-  const image1 = this.document.getElementById("image1");
   // particle system is a collection of small system
 
   class Particle {
-    constructor() {
-      this.x = 0;
-      this.y = 0;
-      this.size = 30;
+    constructor(effect) {
+      this.effect=effect
+      this.x = Math.random()*this.effect.width;
+      this.y = Math.random()*this.effect.height;
+      this.size = 10;
+      // velocity
+      this.vx=1
+      this.vy=1
     }
     draw(context) {
         context.fillRect(this.x, this.y, this.size, this.size);
+    }
+    update(){
+      this.x+=this.vx;
+      this.y+=this.vy
     }
   }
   class Effect {
@@ -22,22 +29,44 @@ window.addEventListener("load", function () {
       this.width = width;
       this.height = height;
       this.particleArray = [];
+      this.image=document.getElementById('image1')
+      this.centerX=this.width*0.5
+      this.centerY=this.height*0.5
+      this.x=this.centerX-this.image.width*0.5
+      this.y=this.centerY-this.image.height*0.5
     }
     init() {
-      this.particleArray.push(new Particle());
+      for(let i=0;i<100;i++){
+        this.particleArray.push(new Particle(this));
+      }
     }
     draw(context) {
       this.particleArray.forEach((particle) => particle.draw(context));
+      context.drawImage(this.image,this.x,this.y)
+    }
+    update(){
+      this.particleArray.forEach((particle) => particle.update());
+
     }
   }
 
 
-  function animate() {
-    // to make it interactive
-  }
+ 
   const effect = new Effect(canvas.width, canvas.height);
   effect.init();
-  effect.draw(ctx);
+
+
+  function animate() {
+    // we need to clear it 
+    ctx.clearRect(0,0,canvas.width,canvas.height)
+    // to make it interactive
+    effect.draw(ctx);
+    effect.update()
+    // built in
+    requestAnimationFrame(animate);
+    // usually 60fps
+  }
+  animate()
 });
 
 // fillRect is built in
